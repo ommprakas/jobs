4b1b7303c9d591c189960b40d9b0ac764453b4b3
ommprakas <124022746+ommprakas@users.noreply.github.com>
2024-08-31 11:20:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-export * from "./dashboard";
-export * from "./jobDetails.screen";
-export * from "./jobSearch.screen";
-export * from "./userProfile.screen";
-export * from "./resumeList";
-export * from "./appliedJobsList.screen";
-export * from "./jobSearchRecruiter.screen";
-export * from "./jobDetailsRecruiter.screen";
-export * from "./SuggestionJobScreen";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./utils/common/i18n";
+import { App } from "./app";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { store } from "./redux";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+import { Helmet } from "react-helmet";
+import "react-widgets/styles.css";
+import i18next from "i18next";
+import { Loading } from "./component";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{i18next.t("site.title")}</title>
+
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://www.dojoko.com/wp-content/uploads/2022/07/cropped-Favicon_white-192x192.png"
+          sizes="192x192"
+        />
+      </Helmet>
+      <App />
+      {/* <Loading /> */}
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ vars.js
@@ -0,0 +1,6 @@
+export const envVariables={
+    REACT_APP_COMMON_SERVICES:"https://api.dojoko.com/cs/common-services/1/v1",
+    REACT_APP_MAILER:"http://localhost:3009/mailer/v1   ",
+    REACT_APP_CAREER:"http://localhost:3010/careers/v1"
+
+}
